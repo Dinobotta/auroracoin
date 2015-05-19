2ae295290845c780e9384981c5f5d0577c69e8d8
Myckel Habets <myckel@sdf.lonestar.org>
2015-05-19 22:20:03 +0200

    support for Icelandic characters

--- src/util.cpp
+++ src/util.cpp
@@ -457,7 +457,7 @@ bool ParseMoney(const char* pszIn, int64& nRet)
 
 // safeChars chosen to allow simple messages/URLs/email addresses, but avoid anything
 // even possibly remotely dangerous like & or >
-static string safeChars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890 .,;_/:?@");
+static string safeChars("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890éÉÝýÚúÍíÓóÐðÁáÆæÖöÞþ .,;_/:?@");
 string SanitizeString(const string& str)
 {
     string strResult;
